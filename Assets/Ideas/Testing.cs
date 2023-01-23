1568cce2d0aa39bcac8d11beee2a789da9f6894b
SusanneKHansson <119973578+SusanneKHansson@users.noreply.github.com>
2023-01-23 15:39:48 +0100

    File structure and test file
    Testing repo between people

---
+++ Assets/Ideas/Testing.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //This is merely a test
+}
